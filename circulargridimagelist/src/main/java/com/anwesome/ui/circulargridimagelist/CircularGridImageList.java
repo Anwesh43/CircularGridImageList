3d63fea83a52e4f0848a81e92710b03966571a9e
Anwesh43 <anweshthecool0@gmail.com>
2017-05-05 09:58:55 +0530

    Adding CircularGridImageList

---
+++ circulargridimagelist/src/main/java/com/anwesome/ui/circulargridimagelist/CircularGridImageList.java
@@ -0,0 +1,32 @@
+package com.anwesome.ui.circulargridimagelist;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+
+/**
+ * Created by anweshmishra on 05/05/17.
+ */
+public class CircularGridImageList  {
+    private Activity activity;
+    private ScrollView scrollView;
+    private CircularGridImageLayout circularGridImageLayout;
+    private boolean isShown = false;
+    public CircularGridImageList(Activity activity) {
+        this.activity = activity;
+        scrollView = new ScrollView(activity);
+        circularGridImageLayout = new CircularGridImageLayout(activity);
+        scrollView.addView(circularGridImageLayout,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+    }
+    public void addImage(Bitmap bitmap) {
+        if(!isShown) {
+            circularGridImageLayout.addImage(bitmap);
+        }
+    }
+    public void show() {
+        if(!isShown) {
+            activity.setContentView(circularGridImageLayout);
+        }
+    }
+}
