236bcaf48959af481788efcdad682261bf7a1eea
Anwesh43 <anweshthecool0@gmail.com>
2017-05-05 08:52:55 +0530

    Adding DrawingController for CircularImageView

--- circulargridimagelist/src/main/java/com/anwesome/ui/circulargridimagelist/CircularImageView.java
+++ circulargridimagelist/src/main/java/com/anwesome/ui/circulargridimagelist/CircularImageView.java
@@ -9,17 +9,38 @@ import android.view.*;
  */
 public class CircularImageView extends View {
     private Bitmap bitmap;
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private DrawingController drawingController = new DrawingController();
     public CircularImageView(Context context, Bitmap bitmap) {
         super(context);
         this.bitmap = bitmap;
     }
     public void onDraw(Canvas canvas) {
-
+        drawingController.draw(canvas,paint);
     }
     public boolean onTouchEvent(MotionEvent event) {
         return true;
     }
     public void update(float factor) {
         postInvalidate();
+    }
+    private class DrawingController {
+        private int render = 0;
+        public void draw(Canvas canvas,Paint paint) {
+            int r = canvas.getWidth()/2;
+            if(render == 0) {
+                bitmap = Bitmap.createScaledBitmap(bitmap,r*2,r*2,true);
+            }
+            canvas.save();
+            canvas.translate(r,r);
+            Path path = new Path();
+            path.addCircle(0,0,r*4/5, Path.Direction.CCW);
+            paint.setColor(Color.WHITE);
+            canvas.drawCircle(0,0,r,paint);
+            canvas.clipPath(path);
+            canvas.drawBitmap(bitmap,-bitmap.getWidth()/2,-bitmap.getHeight()/2,paint);
+            canvas.restore();
+            render++;
+        }
     }
 }
